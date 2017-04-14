a1530869261660a15f6004c6111b5dc64100cb95
Edasaki <x@edasaki.com>
2017-04-14 13:19:20 -0700

    Did a lot of work on multi-configs

--- src/extension.ts
+++ src/extension.ts
@@ -5,23 +5,59 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+let flexRunner = null;
+let last = null;
+
 export function activate(context: vscode.ExtensionContext) {
+    interface KeyValPair {
+        key: string,
+        value: string
+    }
+    let actions: KeyValPair[] = [];
+    let strings: string[] = [];
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "flex-runner-vscode" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let flexRunner = new FlexRunner();
+    flexRunner = new FlexRunner();
     flexRunner.init();
-    let disposable = vscode.commands.registerCommand('flex-runner.run.config.1', () => {
-        flexRunner.test();
+    for (var k = 1; k < 10; k++) {
+        const index = k;
+        const commandName = "flex-runner.run.config." + index;
+        let disposable = vscode.commands.registerCommand(commandName, () => {
+            let config = vscode.workspace.getConfiguration("flex-runner.config." + index);
+            let command = config.get("command");
+            if (command == null) {
+                vscode.window.showErrorMessage("You don't have any command set for configuration " + index + "!");
+            } else {
+                flexRunner.send(command);
+                flexRunner.space();
+            }
+        });
+        context.subscriptions.push(disposable);
+
+        let key = index + " - Run Configuration " + index;
+        strings.push(key);
+        actions[key] = { key: key, value: "flex-runner.run.config." + index };
+    }
+
+    let disposable = vscode.commands.registerCommand('flex-runner.run.config.last', () => {
     });
     context.subscriptions.push(disposable);
-    disposable = vscode.commands.registerCommand('flex-runner.run.config.last', () => {
-        flexRunner.test(); 
+    let key = "0 - Run Last Configuration";
+    strings.unshift(key);
+    actions[key] = { key: key, value: "flex-runner.run.config.last" };
+
+    const options: vscode.QuickPickOptions = {
+        placeHolder: 'Choose a configuration to run'
+    };
+
+    disposable = vscode.commands.registerCommand('flex-runner.chooser', () => {
+        vscode.window.showQuickPick(strings, options).then((resultString) => {
+            if (!resultString) {
+                return;
+            }
+            let command = actions[resultString].value;
+            vscode.commands.executeCommand(command);
+        });
     });
     context.subscriptions.push(disposable);
 }
@@ -39,20 +75,26 @@ class FlexRunner {
         this.send("cls");
     }
 
-    private send(command: string) {
-        this._terminal.sendText(command);
+    public space() {
+        for (var k = 0; k < 2; k++) {
+            this._terminal.sendText("");
+        }
     }
 
-    public test() {
-        this._terminal.sendText("echo test");
+    public send(command: string) {
+        this._terminal.sendText(command);
+        this._terminal.show(true);
     }
 
     public dispose() {
-        this._terminal.dispose(); 
+        this._terminal.dispose();
     }
 
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
+    if (flexRunner != null) {
+        flexRunner.dispose();
+        flexRunner = null;
+    }
 }
